2f7cfc688a709eeb67e61720c3537caf43006510
Paguro <ivankosaev2006@gmail.com>
2023-10-28 10:00:02 +0300

    Design WindowAuthentication

---
+++ RecordSocialServicesProvision/WindowAuthentication.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RecordSocialServicesProvision
+{
+    /// <summary>
+    /// Interaction logic for WindowAuthentication.xaml
+    /// </summary>
+    public partial class WindowAuthentication : Window
+    {
+        public WindowAuthentication()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAuthentication_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
